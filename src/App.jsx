03c2bf83bff26bef6ea66e2f231a5d956d82fcc6
fokhrul272 <fokrulislam1013@gmail.com>
2024-11-25 13:06:31 -0800

    added the blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -6,6 +8,11 @@ function App() {
   return (
     <>
      <Header></Header>
+     <div className='flex'>
+     <Blogs></Blogs>
+     <Bookmarks></Bookmarks>
+     </div>
+     
     </>
   )
 }
